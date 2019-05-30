3ddb5aa07e9297c591b3922d01edd200adc9b382
mick-roper <mick.roper@dazn.com>
2019-05-30 20:40:36 +0100

    added some more flags

--- src/main.go
+++ src/main.go
@@ -9,10 +9,12 @@ func main() {
 	var url string
 	var iterations int
 	var specFile string
+	var concurrency int
 
 	flag.StringVar(&url, "url", "", "the url to be tested")
 	flag.IntVar(&iterations, "iterations", 0, "the number of iterations")
 	flag.StringVar(&specFile, "spec-file", "", "path to the spec file")
+	flag.IntVar(&concurrency, "concurrency", 0, "the number of concurrent HTTP requests that can be made")
 
 	flag.Parse();
 
@@ -20,7 +22,7 @@ func main() {
  
 	// todo: find a better way to provide args
 
-	spec = &core.TestSpec{URL: url, Iterations: iterations, Concurrency: 2}
+	spec = &core.TestSpec{URL: url, Iterations: iterations, Concurrency: concurrency}
 
 	core.Run(spec)
 }
