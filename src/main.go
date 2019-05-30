09b3c82dd655e8cbf477bc05082a00f3741ba632
mick-roper <mick.roper@dazn.com>
2019-05-30 16:42:44 +0100

    addded std dev

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"math"
 )
 
 func main() {
@@ -13,7 +14,7 @@ func main() {
 	fmt.Printf("request\tstatus code\telapsed\n")
 
 	iterations := 100
-	responseTimes := make([]int64, iterations)
+	responseTimes := make([]float64, iterations)
 
 	for i := 0; i < iterations; i++ {
 		start := time.Now().UnixNano()
@@ -21,7 +22,7 @@ func main() {
 		resp, err := client.Get(url)
 
 		end := time.Now().UnixNano()
-		elapsedMs := (end - start) / 1000000
+		elapsedMs := (float64)((end - start) / 1000000)
 		responseTimes[i] = elapsedMs
 
 		if err != nil { 
@@ -40,8 +41,8 @@ func main() {
 	fmt.Printf("avg response time: %vms\n", avg)
 }
 
-func getMax(x []int64) int64 {
-	var m int64
+func getMax(x []float64) float64 {
+	var m float64
 
 	for i := 0; i < len(x); i++ {
 		if x[i] > m {
@@ -52,8 +53,8 @@ func getMax(x []int64) int64 {
 	return m
 }
 
-func getMin(x []int64) int64 {
-	var m int64 = 10000000000 // arbitrarily large number
+func getMin(x []float64) float64 {
+	var m float64 = 10000000000 // arbitrarily large number
 
 	for i := 0; i < len(x); i++ {
 		if x[i] < m {
@@ -64,7 +65,7 @@ func getMin(x []int64) int64 {
 	return m
 }
 
-func getAvg(x []int64) float64 {
+func getAvg(x []float64) float64 {
 	l := (float64)(len(x))
 	var sum float64
 
@@ -73,4 +74,19 @@ func getAvg(x []int64) float64 {
 	}
 
 	return sum / l
+}
+
+func getStdDev(x []float64) float64 {
+	avg := getAvg(x)
+
+	results := make([]float64, len(x))
+
+	for i := 0; i < len(x); i++ {
+		n := ((x[i]) - avg) * ((x[i]) - avg)
+		results[i] = n
+	}
+
+	avg = getAvg(results)
+
+	return math.Sqrt(avg)
 }
