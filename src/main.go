49bd4d912c9096f099e5fcfd6dfc0f484b1e112c
mick-roper <mick.roper@dazn.com>
2019-05-30 16:11:19 +0100

    super simple impl done

--- src/main.go
+++ src/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	log.Print("hello world")
+	const url = "https://api.marcopolo.acc.dazn-dev.com/v1/override/1bab7192-92f6-4ca0-b0f0-29a67b275537/geofence"
+	client := &http.Client{}
+
+	for i := 0; i < 10; i++ {
+		log.Printf("making GET request %v", i)
+
+		start := time.Now().UnixNano()
+
+		client.Get(url)
+
+		end := time.Now().UnixNano()
+
+		log.Printf("request %v took %vms", i, end - start)
+	}
 }
