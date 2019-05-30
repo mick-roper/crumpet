e525ce6036572b086a91e88e508876347e4557f4
mick-roper <mick.roper@dazn.com>
2019-05-30 19:38:27 +0100

    added some flags

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"flag"
 	"core"
 )
@@ -8,13 +9,19 @@ import (
 func main() {
 	var url string
 	var iterations int
+	var specFile string
 
 	flag.StringVar(&url, "url", "", "the url to be tested")
 	flag.IntVar(&iterations, "iterations", 0, "the number of iterations")
+	flag.StringVar(&specFile, "spec-file", "", "path to the spec file")
 
 	flag.Parse();
 
-	spec := &core.TestSpec{URL: url, Iterations: iterations}
+	var spec *core.TestSpec
+ 
+	// todo: find a better way to provide args
+
+	spec = &core.TestSpec{URL: url, Iterations: iterations}
 
 	core.Run(spec)
 }
