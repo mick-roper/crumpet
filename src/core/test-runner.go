01221b929c10bbfa08af4d4c95e37e27dbf8c6f8
mick-roper <mick.roper@dazn.com>
2019-05-30 20:49:26 +0100

    added some more messaging

--- src/core/test-runner.go
+++ src/core/test-runner.go
@@ -70,6 +70,7 @@ func Run(spec *TestSpec) {
 
 	min, max, avg, stdDev := getMin(responseTimes), getMax(responseTimes), getAvg(responseTimes), getStdDev(responseTimes)
 
+	fmt.Printf("requests: %v\n", iterations)
 	fmt.Printf("min response time: %vms\n", min)
 	fmt.Printf("max response time: %vms\n", max)
 	fmt.Printf("avg response time: %6.2fms\n", avg)
