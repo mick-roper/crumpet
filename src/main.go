915ef15e09915da15872a4acbf2bd6f3c668dabc
mick-roper <mick.roper@dazn.com>
2019-05-30 16:01:40 +0100

    creating simple implementation

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
-import "log"
 
 func main() {
-	log.Print("hello world")
+	const url = "https://api.marcopolo.acc.dazn-dev.com/v1/override/1bab7192-92f6-4ca0-b0f0-29a67b275537/geofence"
+
 }
