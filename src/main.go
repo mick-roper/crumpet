4a13497f361adb6630d73bb77a7375da6ba4ad94
mick-roper <mick.roper@dazn.com>
2019-05-31 09:19:40 +0100

    added a mechanism to test multiple paths

--- src/main.go
+++ src/main.go
@@ -3,27 +3,31 @@ package main
 import (
 	"flag"
 	"core"
+	"strings"
 )
 
 func main() {
-	var url string
+	var host string
 	var iterations int
 	var specFile string
 	var concurrency int
 	var maxDelayMs int
+	var paths string
 
-	flag.StringVar(&url, "url", "", "the url to be tested")
+	flag.StringVar(&host, "host", "", "the host to be tested")
 	flag.IntVar(&iterations, "iterations", 0, "the number of iterations")
 	flag.StringVar(&specFile, "spec-file", "", "path to the spec file")
 	flag.IntVar(&concurrency, "concurrency", 1, "the number of concurrent HTTP requests that can be made")
-	flag.IntVar(&maxDelayMs, "max-delay", 0, "The maximum amount of delay in milliseconds")
+	flag.IntVar(&maxDelayMs, "max-delay", 0, "the maximum amount of delay in milliseconds")
+	flag.StringVar(&paths, "paths", "", "comma separated collection of paths to test")
 
 	flag.Parse();
 
 	var spec *core.TestSpec
 
 	spec = &core.TestSpec{
-		URL: url, 
+		Host: host, 
+		Paths: strings.Split(paths, ","),
 		Iterations: iterations, 
 		Concurrency: concurrency,
 		MaxDelayMs: maxDelayMs,
