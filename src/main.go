f96333d3aecab244a2c489748ee28d1cf8b7d2f3
mick-roper <mick.roper@dazn.com>
2019-05-31 08:50:17 +0100

    updated the defaults

--- src/main.go
+++ src/main.go
@@ -15,7 +15,7 @@ func main() {
 	flag.StringVar(&url, "url", "", "the url to be tested")
 	flag.IntVar(&iterations, "iterations", 0, "the number of iterations")
 	flag.StringVar(&specFile, "spec-file", "", "path to the spec file")
-	flag.IntVar(&concurrency, "concurrency", 0, "the number of concurrent HTTP requests that can be made")
+	flag.IntVar(&concurrency, "concurrency", 1, "the number of concurrent HTTP requests that can be made")
 	flag.IntVar(&maxDelayMs, "max-delay", 0, "The maximum amount of delay in milliseconds")
 
 	flag.Parse();
