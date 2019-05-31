97a3baecdae69e7d02d1902fedfe5a4eacd57393
mick-roper <mick.roper@dazn.com>
2019-05-31 16:34:29 +0100

    added 99.9th percentile to results

--- src/core/test-spec.go
+++ src/core/test-spec.go
@@ -43,6 +43,7 @@ func (t *TestResult) Print() {
 	fmt.Printf("90th percentile:\t%6.2fms\n", t.AverageElapsedMs90thPc())
 	fmt.Printf("95th percentile:\t%6.2fms\n", t.AverageElapsedMs95thPc())
 	fmt.Printf("99th percentile:\t%6.2fms\n", t.AverageElapsedMs99thPc())
+	fmt.Printf("99.9th percentile:\t%6.2fms\n", t.AverageElapsedMs999thPc())
 }
 
 // RequestCount returns the number of requests
@@ -68,6 +69,11 @@ func (t *TestResult) AverageElapsedMs95thPc() float64 {
 // AverageElapsedMs99thPc gets the 99th percentile average response time
 func (t *TestResult) AverageElapsedMs99thPc() float64 {
 	return getPercentileAverage(t.responses, 0.99)
+}
+
+// AverageElapsedMs999thPc gets the 99.9th percentile average response time
+func (t *TestResult) AverageElapsedMs999thPc() float64 {
+	return getPercentileAverage(t.responses, 0.999)
 }
 
 // MaxElapsedMs returns the maximum elapsed ms
